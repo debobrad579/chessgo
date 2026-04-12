296932fe04ecec3d00687e9130d7e769a3548da0
Brady DeBoer <bradydeboer195@gmail.com>
2026-04-12 17:18:51 -0400

    fix useWindowSize

--- app/hooks/useWindowSize.ts
+++ app/hooks/useWindowSize.ts
@@ -1,17 +1,21 @@
 import { useSyncExternalStore } from "react"
 
-function getSnapshot() {
-  return {
-    width: window.innerWidth,
-    height: window.innerHeight,
-  }
-}
-
 function subscribe(callback: () => void) {
   window.addEventListener("resize", callback)
   return () => window.removeEventListener("resize", callback)
 }
 
+function getWidth() {
+  return window.innerWidth
+}
+
+function getHeight() {
+  return window.innerHeight
+}
+
 export function useWindowSize() {
-  return useSyncExternalStore(subscribe, getSnapshot)
+  const width = useSyncExternalStore(subscribe, getWidth)
+  const height = useSyncExternalStore(subscribe, getHeight)
+
+  return { width, height }
 }
