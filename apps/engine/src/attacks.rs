03dba06ac93c08e0ce70001960b2d73784cf1d75
Brady DeBoer <bradydeboer195@gmail.com>
2026-05-16 23:04:05 -0400

    add attack table tests

--- apps/engine/src/attacks.rs
+++ apps/engine/src/attacks.rs
@@ -1,5 +1,5 @@
-const NOT_A: u64 = 0xfefefefefefefefe;
-const NOT_H: u64 = 0x7f7f7f7f7f7f7f7f;
+const NOT_A: u64 = 0xFEFEFEFEFEFEFEFE;
+const NOT_H: u64 = 0x7F7F7F7F7F7F7F7F;
 const NOT_AB: u64 = 0xFCFCFCFCFCFCFCFC;
 const NOT_GH: u64 = 0x3F3F3F3F3F3F3F3F;
 
@@ -12,11 +12,11 @@ const fn compute_pawn_attacks(is_black: bool) -> [u64; 64] {
         let mut attacks = 0u64;
 
         if is_black {
-            attacks |= (bit & NOT_A) >> 7; // south-east
-            attacks |= (bit & NOT_H) >> 9; // south-west
+            attacks |= (bit & NOT_H) >> 7; // south-east
+            attacks |= (bit & NOT_A) >> 9; // south-west
         } else {
-            attacks |= (bit & NOT_H) << 7; // north-west
-            attacks |= (bit & NOT_A) << 9; // north-east
+            attacks |= (bit & NOT_H) << 9; // north-east
+            attacks |= (bit & NOT_A) << 7; // north-west
         }
 
         table[i] = attacks;
@@ -60,15 +60,15 @@ const fn compute_king_attacks() -> [u64; 64] {
         let mut attacks = 0u64;
 
         attacks |= bit << 8; // north
-        attacks |= (bit & NOT_H) << 7; // north-west
-        attacks |= (bit & NOT_A) << 9; // north-east
+        attacks |= (bit & NOT_A) << 7; // north-west
+        attacks |= (bit & NOT_H) << 9; // north-east
 
         attacks |= bit >> 8; // south
-        attacks |= (bit & NOT_A) >> 7; // south-east
-        attacks |= (bit & NOT_H) >> 9; // south-west
+        attacks |= (bit & NOT_A) >> 9; // south-west
+        attacks |= (bit & NOT_H) >> 7; // south-east
 
-        attacks |= (bit & NOT_A) << 1; // east
-        attacks |= (bit & NOT_H) >> 1; // west
+        attacks |= (bit & NOT_H) << 1; // east
+        attacks |= (bit & NOT_A) >> 1; // west
 
         table[i] = attacks;
         i += 1;
@@ -81,3 +81,45 @@ pub static WHITE_PAWN_ATTACKS: [u64; 64] = compute_pawn_attacks(false);
 pub static BLACK_PAWN_ATTACKS: [u64; 64] = compute_pawn_attacks(true);
 pub static KNIGHT_ATTACKS: [u64; 64] = compute_knight_attacks();
 pub static KING_ATTACKS: [u64; 64] = compute_king_attacks();
+
+#[cfg(test)]
+mod test {
+    use crate::attacks::{BLACK_PAWN_ATTACKS, KING_ATTACKS, KNIGHT_ATTACKS, WHITE_PAWN_ATTACKS};
+
+    #[test]
+    fn white_pawn() {
+        assert_eq!(WHITE_PAWN_ATTACKS[28], 0x0000002800000000); // e4
+        assert_eq!(WHITE_PAWN_ATTACKS[31], 0x0000004000000000); // h4
+        assert_eq!(WHITE_PAWN_ATTACKS[24], 0x0000000200000000); // a4
+    }
+
+    #[test]
+    fn black_pawn() {
+        assert_eq!(BLACK_PAWN_ATTACKS[36], 0x0000000028000000); // e5
+        assert_eq!(BLACK_PAWN_ATTACKS[39], 0x0000000040000000); // h5
+        assert_eq!(BLACK_PAWN_ATTACKS[32], 0x0000000002000000); // a5
+    }
+
+    #[test]
+    fn knight() {
+        assert_eq!(KNIGHT_ATTACKS[18], 0x0000000A1100110A); // c3
+        assert_eq!(KNIGHT_ATTACKS[45], 0x5088008850000000); // f6
+        assert_eq!(KNIGHT_ATTACKS[0], 0x0000000000020400); // a1
+        assert_eq!(KNIGHT_ATTACKS[1], 0x0000000000050800); // b1
+        assert_eq!(KNIGHT_ATTACKS[7], 0x0000000000402000); // h1
+        assert_eq!(KNIGHT_ATTACKS[6], 0x0000000000A01000); // g1
+        assert_eq!(KNIGHT_ATTACKS[22], 0x000000A0100010A0); // g3
+        assert_eq!(KNIGHT_ATTACKS[41], 0x0508000805000000); // b6
+    }
+
+    #[test]
+    fn king() {
+        assert_eq!(KING_ATTACKS[28], 0x0000003828380000); // e4
+        assert_eq!(KING_ATTACKS[6], 0x000000000000E0A0); // g1
+        assert_eq!(KING_ATTACKS[7], 0x000000000000C040); // h1
+        assert_eq!(KING_ATTACKS[57], 0x0507000000000000); // b8
+        assert_eq!(KING_ATTACKS[56], 0x0203000000000000); // a8
+        assert_eq!(KING_ATTACKS[31], 0x000000C040C00000); // h4
+        assert_eq!(KING_ATTACKS[24], 0x0000000302030000); // a4
+    }
+}
