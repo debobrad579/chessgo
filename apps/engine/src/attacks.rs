f45af44c8daad275b799b20fc4e407648e43efd0
Brady DeBoer <bradydeboer195@gmail.com>
2026-05-16 22:02:36 -0400

    compute pawn attacks

--- apps/engine/src/attacks.rs
+++ apps/engine/src/attacks.rs
@@ -3,6 +3,29 @@ const NOT_H: u64 = 0x7f7f7f7f7f7f7f7f;
 const NOT_AB: u64 = 0xFCFCFCFCFCFCFCFC;
 const NOT_GH: u64 = 0x3F3F3F3F3F3F3F3F;
 
+const fn compute_pawn_attacks(is_black: bool) -> [u64; 64] {
+    let mut table = [0u64; 64];
+
+    let mut i = 0;
+    while i < 64 {
+        let bit = 1u64 << i;
+        let mut attacks = 0u64;
+
+        if is_black {
+            attacks |= (bit & NOT_A) >> 7; // south-east
+            attacks |= (bit & NOT_H) >> 9; // south-west
+        } else {
+            attacks |= (bit & NOT_H) << 7; // north-west
+            attacks |= (bit & NOT_A) << 9; // north-east
+        }
+
+        table[i] = attacks;
+        i += 1;
+    }
+
+    return table;
+}
+
 const fn compute_knight_attacks() -> [u64; 64] {
     let mut table = [0u64; 64];
 
@@ -37,14 +60,15 @@ const fn compute_king_attacks() -> [u64; 64] {
         let mut attacks = 0u64;
 
         attacks |= bit << 8; // north
-        attacks |= (bit & NOT_A) << 1; // east
         attacks |= (bit & NOT_H) << 7; // north-west
         attacks |= (bit & NOT_A) << 9; // north-east
 
         attacks |= bit >> 8; // south
-        attacks |= (bit & NOT_H) >> 1; // west
         attacks |= (bit & NOT_A) >> 7; // south-east
         attacks |= (bit & NOT_H) >> 9; // south-west
+
+        attacks |= (bit & NOT_A) << 1; // east
+        attacks |= (bit & NOT_H) >> 1; // west
 
         table[i] = attacks;
         i += 1;
@@ -53,5 +77,7 @@ const fn compute_king_attacks() -> [u64; 64] {
     return table;
 }
 
+pub static WHITE_PAWN_ATTACKS: [u64; 64] = compute_pawn_attacks(false);
+pub static BLACK_PAWN_ATTACKS: [u64; 64] = compute_pawn_attacks(true);
 pub static KNIGHT_ATTACKS: [u64; 64] = compute_knight_attacks();
 pub static KING_ATTACKS: [u64; 64] = compute_king_attacks();
