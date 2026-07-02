c43e05bc857a781bc5f16c7a85be0db4bbcf9e39
Brady DeBoer <bradydeboer195@gmail.com>
2026-07-02 15:13:34 -0400

    add margin to service unavailable error

--- apps/app/src/components/errors/ServiceUnavailable.tsx
+++ apps/app/src/components/errors/ServiceUnavailable.tsx
@@ -1,6 +1,6 @@
 export function ServiceUnavailable() {
   return (
-    <h1 className="text-center text-4xl font-bold">
+    <h1 className="mt-4 text-center text-4xl font-bold">
       503 - Service Unavailable
     </h1>
   )
